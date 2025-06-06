f27592417d33df6ce35269d4a12e731cfbd796dc
Freddy Tello <ftellocrespo@hotmail.com>
2025-06-05 20:39:07 -0500

    hydra

--- src/data_eng/stage0_loading.py
+++ src/data_eng/stage0_loading.py
@@ -7,6 +7,9 @@ import requests
 from app_logging import logging
 from app_exception.exception import AppException
 
+import hydra
+from omegaconf import DictConfig
+
 class GetData:
     
     '''
@@ -17,18 +20,18 @@ class GetData:
     def __init__(self):
         pass
 
-    def get_data(self, config_path):
+    def get_data(self, config):
         try:
             logging.info("Getting the data from the external source")
 
             # URL del archivo CSV en GitHub
             # aquí se asume que config_path es la URL del archivo CSV
-            github_csv_url = config_path
+            github_csv_url = config.data_source.url
             
             # Ruta local donde se guardará el archivo
-            self.external_data = "data/external"
+            self.external_data = config.data_source.external_data_dir
             os.makedirs(self.external_data, exist_ok=True)
-            local_path = os.path.join(self.external_data, "Consignment_pricing.csv")
+            local_path = os.path.join(self.external_data, config.data_source.filename)
 
             # Descargar el archivo CSV desde GitHub
             response = requests.get(github_csv_url)
@@ -52,11 +55,17 @@ class GetData:
             raise AppException(e, sys) from e
 
 
+@hydra.main(config_path=f"{os.getcwd()}/configs", config_name="data_eng", version_base=None)
+def main(cfg: DictConfig):
+    logging.basicConfig(level=logging.INFO)
+    data = GetData().get_data(cfg)
 
 if __name__ == "__main__":
+    main()
 
-    args = argparse.ArgumentParser()
-    args.add_argument('--input_path', default="https://raw.githubusercontent.com/jmem-ec/KRRCourse/ccbd6ccf8389ba0988d53fc9300a64da00e6368b/Consignment_pricing.csv")
+# if __name__ == "__main__":
+#    args = argparse.ArgumentParser()
+#    args.add_argument('--input_path', default="https://raw.githubusercontent.com/jmem-ec/KRRCourse/ccbd6ccf8389ba0988d53fc9300a64da00e6368b/Consignment_pricing.csv")
     
-    parsed_args = args.parse_args()
-    data = GetData().get_data(config_path=parsed_args.input_path)
+#    parsed_args = args.parse_args()
+#    data = GetData().get_data(config_path=parsed_args.input_path)
